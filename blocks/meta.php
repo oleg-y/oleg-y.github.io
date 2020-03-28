9e21d992e74785b396840c3fb81ff75e32b248f6
oleg-y <oleg@mail2k.ru>
2020-03-29 00:25:25 +0200

    Add files via upload

---
+++ blocks/meta.php
@@ -0,0 +1,61 @@
+	<!-- Google Analytics counter -->
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114510100-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'UA-114510100-1');
+	</script>
+
+	<!-- /Google Analytics counter -->
+
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript">
+		(function(d, w, c) {
+			(w[c] = w[c] || []).push(function() {
+				try {
+					w.yaCounter47748916 = new Ya.Metrika({
+						id: 47748916,
+						clickmap: true,
+						trackLinks: true,
+						accurateTrackBounce: true
+					});
+				} catch (e) {}
+			});
+			var n = d.getElementsByTagName("script")[0],
+				s = d.createElement("script"),
+				f = function() {
+					n.parentNode.insertBefore(s, n);
+				};
+			s.type = "text/javascript";
+			s.async = true;
+			s.src = "https://mc.yandex.ru/metrika/watch.js";
+			if (w.opera == "[object Opera]") {
+				d.addEventListener("DOMContentLoaded", f, false);
+			} else {
+				f();
+			}
+		})(document, window, "yandex_metrika_callbacks");
+	</script> <noscript><div><img src="https://mc.yandex.ru/watch/47748916" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
+
+	<!-- Rating@Mail.ru counter -->
+	<script type="text/javascript">
+	var _tmr = window._tmr || (window._tmr = []);
+	_tmr.push({id: "3132597", type: "pageView", start: (new Date()).getTime()});
+	(function (d, w, id) {
+	  if (d.getElementById(id)) return;
+	  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+	  ts.src = "https://top-fwz1.mail.ru/js/code.js";
+	  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+	  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+	})(document, window, "topmailru-code");
+	</script><noscript><div>
+	<img src="https://top-fwz1.mail.ru/counter?id=3132597;js=na" style="border:0;position:absolute;left:-9999px;" alt="Top.Mail.Ru" />
+	</div></noscript>
+	<!-- //Rating@Mail.ru counter -->
+
+	
+
